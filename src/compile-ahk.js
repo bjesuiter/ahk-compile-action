64fbc72ada19523635804e83b3e21a9bca170bf2
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-04-12 19:23:08 +0200

    improve error handling

--- src/compile-ahk.js
+++ src/compile-ahk.js
@@ -14,14 +14,21 @@ const path = require('path');
 
         const compileParams = ['/in', inScript];
 
+        let stdout = "";
+        let stderr = "";
+
         await exec.exec(path.join(githubActionPath, 'AutoHotkey_1.1.33.06', 'Compiler', 'Ahk2Exe.exe' ), compileParams, {
           stdout: (data) => {
-            process.stdout.write(data.toString());
+            stdout += data.toString();
           },
           stderr: (data) => {
-            process.stderr.write(data.toString());
+            stderr += data.toString();
           },
         });
+
+        if (stderr.length > 0) {
+          core.setFailed(stderr);
+        }
 
         core.setOutput('out_exe', `${path.basename(inScript)}.exe`)
     } catch (error) {
