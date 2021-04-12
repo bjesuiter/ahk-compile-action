d05b9ea8f6f03d60bae45762b65f6bdb52b0bc32
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-04-12 13:34:51 +0200

    add js file to compile AHK Script

---
+++ compile-ahk.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const path = require('path');
+
+/**
+ * Github Core Docs: https://github.com/actions/toolkit/tree/main/packages/core
+ * Github Exec Docs: https://github.com/actions/toolkit/tree/main/packages/exec
+ */
+
+ async function run() {
+    try {
+        const inScript = core.getInput('in_script', { required: true });
+        const githubActionPath = __dirname;
+
+        const compileParams = ['/in', inScript];
+
+        await exec.exec(path.join(githubActionPath, 'AutoHotkey_1.1.33.06', 'Compiler', 'Ahk2Exe.exe' ), compileParams);
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+  }
+  
+  run()
